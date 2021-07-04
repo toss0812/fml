7af1d816b557f66a3230639ac83c50ed42cc2aec
toss0812 <t.dingenouts@live.com>
2021-07-04 17:52:52 +0200

    fixed checksum bug

--- usb_gps_website_update.py
+++ usb_gps_website_update.py
@@ -5,10 +5,9 @@ import serial
 import json
 import time
 
-ser = serial.Serial('COM3', 9600)
-sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser))
+ser = serial.Serial('COM3', 9600) # the serial port of the device -> change to /dev/ttyACM0 when using on pi
+sio = io.TextIOWrapper(io.BufferedRWPair(ser,ser)) # serial reader buffer object
 buoy_id = 1
-
 
 
 while True:
@@ -16,19 +15,18 @@ while True:
     if strip.find(b'GGA') > 0: # exclude all entries not starting with "GGA"
         msg = pynmea2.parse(strip.decode('utf-8')) # parse the incoming serial string to a "Location"-Object
         print(msg) # print data from "Location"-Object
-        r = requests.post(
-            'https://test.regattatracker.nl/api/buoys/' + str(buoy_id) + '/positions', 
-            data = {
-                'api_key': '661876ad034b008a1dee9eecf024c9db',            
-                'latitude': round(msg.latitude, 6),
-                'longitude': round(msg.longitude, 6)
-            }
-        )
+        try: # added try catch construction to catch a bug regarding checksum
+            r = requests.post( # construct the post request
+                'https://test.regattatracker.nl/api/buoys/' + str(buoy_id) + '/positions', 
+                data = {
+                    'api_key': '661876ad034b008a1dee9eecf024c9db',            
+                    'latitude': round(msg.latitude, 6),
+                    'longitude': round(msg.longitude, 6)
+                }
+            )
+        except Exception: # catch any exception regarding the post request
+            pass
+
         print(r.text)
         
-        time.sleep(5)
-        
-        
-
-
-
+        time.sleep(5) # limit the amount of updates, can be inproved with a duty cycle-like contruction
